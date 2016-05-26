2b58eca3808f6e567313990ae48c41fc40a1ada3
Daniel Beaven <dan@Daniels-MacBook-Air.local>
2016-05-26 17:43:51 +0100

    Added fractal

--- script.js
+++ script.js
@@ -28,6 +28,18 @@ document.getElementById('tetrahedron').onclick = function() {
   currentShape = new Tetrahedron([0, 0, 0], (pageHeight / 2) - 50, ctx);
 }
 
+document.getElementById('fractal').onclick = function() {
+  var cubes = [];
+  var cube = new Cube([0,0,0], 400, ctx);
+  cubes.push(cube);
+  for (var i = 0; i < 3; i++) {
+    cubes.forEach(function(c) {
+      cubes = [].concat(cubes, generateInnerSponge(c, 3, 2))
+    })
+  }
+  currentShape = cubes;
+}
+
 // Remove prompt message on click
 document.onclick = function() {
   var prompt = document.getElementById('prompt');
@@ -37,11 +49,21 @@ document.onclick = function() {
 // Refresh canvas animation loop
 function update() {
   ctx.clearRect(-window.innerWidth / 2, -window.innerHeight / 2, window.innerWidth, window.innerHeight);
-  if(run) {
-    currentShape.rotation[0] += rotation[0];
-    currentShape.rotation[1] += rotation[1];
+  if (Array.isArray(currentShape)) {
+    currentShape.forEach(function(shape) {
+      if (run) {
+        shape.rotation[0] += rotation[0];
+        shape.rotation[1] += rotation[1];
+      }
+      shape.update();
+    })
+  } else {
+    if(run) {
+      currentShape.rotation[0] += rotation[0];
+      currentShape.rotation[1] += rotation[1];
+    }
+    currentShape.update();
   }
-  currentShape.update();
   window.requestAnimationFrame(update)
 }
 
